e5f1c828f7f2be8e38a7241eea859de87155672a
Jesse <lenjoyshow@gmail.com>
2017-07-12 21:49:12 +0800

    ftr(dep): add react

---
+++ react/app.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+import "./styles/app.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
