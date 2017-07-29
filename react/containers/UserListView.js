de8287981c195fb62758d1e8564adfe5ae558083
Jesse <lenjoyshow@gmail.com>
2017-07-29 20:42:19 +0800

    ftr(view) config text editable component

--- react/containers/UserListView.js
+++ react/containers/UserListView.js
@@ -2,7 +2,7 @@ import React from "react";
 import {resetPassword} from "../actions/user";
 import {connect} from "react-redux";
 import {Popconfirm, Table} from "antd";
-import UserListItemView from "../components/ListItemView";
+import TextEditableItemView, {TextEditableMode, TextEditableStatus} from "../components/TextEditableItemView";
 import {homeStyle} from "./styles/home";
 
 
@@ -34,37 +34,110 @@ class UserListView extends React.Component {
         };
     }
 
-    renderColumns(data, index, key, text) {
+    render() {
+        const {data} = this.state;
+        const dataSource = data.map((item) => {
+            const obj = {};
+            Object.keys(item).forEach((key) => {
+                obj[key] = key === 'key' ? item[key] : item[key].value;
+            });
+            return obj;
+        });
+
+        const columns = this._buildColumns();
+
+        return (
+            <div style={homeStyle.view_content}>
+                <Table bordered dataSource={dataSource} columns={columns} pagination={{ pageSize: 10 }}/>
+            </div>
+        );
+    }
+
+    /**
+     * 根据数据构建出各个列
+     * @private
+     */
+    _buildColumns() {
+        return [{
+            title: '账号',
+            dataIndex: 'account',
+            width: '15%',
+            render: (text, record, index) => this._buildTextInputColumnItem(this.state.data, index, 'account', text),
+        }, {
+            title: '昵称',
+            dataIndex: 'nickname',
+            width: '15%',
+            render: (text, record, index) => this._buildTextInputColumnItem(this.state.data, index, 'nickname', text),
+        }, {
+            title: '权限',
+            dataIndex: 'auth',
+            width: '15%',
+            render: (text, record, index) => this._buildTextInputColumnItem(this.state.data, index, 'auth', text),
+        }, {
+            title: '所属项目',
+            dataIndex: 'projects',
+            width: '15%',
+            render: (text, record, index) => this._buildTextInputColumnItem(this.state.data, index, 'projects', text),
+        }, {
+            title: '行为',
+            dataIndex: 'operation',
+            render: (text, record, index) => {
+                const {editable} = this.state.data[index].account;
+                return (
+                    <div className="editable-row-operations">
+                        {
+                            editable ?
+                                <span>
+                                    <a onClick={() => this._saveOrCancelItem(index, TextEditableStatus.SAVE)}>Save</a>
+                                    <Popconfirm title="Sure to cancel?"
+                                                onConfirm={() => this._saveOrCancelItem(index, TextEditableStatus.CANCEL)}>
+                                        <a>Cancel</a>
+                                    </Popconfirm>
+                                </span>
+                                :
+                                <span style={{display: 'flex', justifyContent: 'space-around'}}>
+                                    <a onClick={() => this._editItem(index)}>修改昵称</a>
+                                    <a onClick={() => this._editItem(index)}>重置密码</a>
+                                    <a onClick={() => this._editItem(index)}>删除用户</a>
+                                </span>
+                        }
+                    </div>
+                );
+            },
+        }];
+    }
+
+    /**
+     * 构建表格最小元素, 模式为 text/input
+     * @param data
+     * @param index
+     * @param key
+     * @param text
+     * @returns {*}
+     * @private
+     */
+    _buildTextInputColumnItem(data, index, key, text) {
         const {editable, status} = data[index][key];
         if (typeof editable === 'undefined') {
             return text;
         }
         return (
-            <UserListItemView
+            <TextEditableItemView
                 editable={editable}
+                mode={TextEditableMode.INPUT}
                 value={text}
-                onChange={value => this.handleChange(key, index, value)}
                 status={status}
+                onChange={value => this._handleChange(key, index, value)}
             />);
     }
 
-    handleChange(key, index, value) {
-        const {data} = this.state;
-        data[index][key].value = value;
-        this.setState({data});
-    }
-
-    edit(index) {
-        const {data} = this.state;
-        Object.keys(data[index]).forEach((item) => {
-            if (data[index][item] && typeof data[index][item].editable !== 'undefined') {
-                data[index][item].editable = true;
-            }
-        });
-        this.setState({data});
-    }
-
-    editDone(index, type) {
+    /**
+     * 保存或取消item的修改
+     * @param index
+     * @param type
+     * @private
+     */
+    _saveOrCancelItem(index, type) {
         const {data} = this.state;
         Object.keys(data[index]).forEach((item) => {
             if (data[index][item] && typeof data[index][item].editable !== 'undefined') {
@@ -81,66 +154,32 @@ class UserListView extends React.Component {
         });
     }
 
-    render() {
+    /**
+     * 根据index 修改行数据
+     * @param index
+     * @private
+     */
+    _editItem(index) {
         const {data} = this.state;
-        const dataSource = data.map((item) => {
-            const obj = {};
-            Object.keys(item).forEach((key) => {
-                obj[key] = key === 'key' ? item[key] : item[key].value;
-            });
-            return obj;
+        Object.keys(data[index]).forEach((item) => {
+            if (data[index][item] && typeof data[index][item].editable !== 'undefined') {
+                data[index][item].editable = true;
+            }
         });
-        const columns = [{
-            title: '账号',
-            dataIndex: 'account',
-            width: '15%',
-            render: (text, record, index) => this.renderColumns(this.state.data, index, 'account', text),
-        }, {
-            title: '昵称',
-            dataIndex: 'nickname',
-            width: '15%',
-            render: (text, record, index) => this.renderColumns(this.state.data, index, 'nickname', text),
-        }, {
-            title: '权限',
-            dataIndex: 'auth',
-            width: '15%',
-            render: (text, record, index) => this.renderColumns(this.state.data, index, 'auth', text),
-        }, {
-            title: '所属项目',
-            dataIndex: 'projects',
-            width: '15%',
-            render: (text, record, index) => this.renderColumns(this.state.data, index, 'projects', text),
-        }, {
-            title: '行为',
-            dataIndex: 'operation',
-            render: (text, record, index) => {
-                const {editable} = this.state.data[index].account;
-                return (
-                    <div className="editable-row-operations">
-                        {
-                            editable ?
-                                <span>
-                                    <a onClick={() => this.editDone(index, 'save')}>Save</a>
-                                    <Popconfirm title="Sure to cancel?" onConfirm={() => this.editDone(index, 'cancel')}>
-                                        <a>Cancel</a>
-                                    </Popconfirm>
-                                </span>
-                                :
-                                <span style={{display: 'flex', justifyContent: 'space-around'}}>
-                                    <a onClick={() => this.edit(index)}>修改昵称</a>
-                                    <a onClick={() => this.edit(index)}>重置密码</a>
-                                    <a onClick={() => this.edit(index)}>删除用户</a>
-                                </span>
-                        }
-                    </div>
-                );
-            },
-        }];
-        return (
-            <div style={homeStyle.view_content}>
-                <Table bordered dataSource={dataSource} columns={columns}/>
-            </div>
-        );
+        this.setState({data});
+    }
+
+    /**
+     * 用户数据变化后回调
+     * @param key
+     * @param index
+     * @param value
+     * @private
+     */
+    _handleChange(key, index, value) {
+        const {data} = this.state;
+        data[index][key].value = value;
+        this.setState({data});
     }
 }
 function select(state) {
